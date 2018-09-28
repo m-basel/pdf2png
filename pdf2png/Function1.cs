f2c19ec00ca96baf943d311b7d095c156ae9b2ce
martin-basel <mbasel35@gmail.com>
2018-09-28 11:59:26 +0200

    bit refactored

--- pdf2png/Function1.cs
+++ pdf2png/Function1.cs
@@ -5,6 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Ghostscript.NET.Rasterizer;
+using System.Drawing;
+
+using System.IO;
+using System.Drawing.Imaging;
+using System;
 
 namespace pdf2png
 {
@@ -13,23 +19,50 @@ namespace pdf2png
         [FunctionName("Function1")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
-            log.Info("C# HTTP trigger function processed a request.");
-
-            // parse query parameter
-            string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
-                .Value;
-
-            if (name == null)
+            var options = new Options()
             {
-                // Get request body
-                dynamic data = await req.Content.ReadAsAsync<object>();
-                name = data?.name;
+                Input = DownloadFile(GetInputFileFromUrl(req))
+            };
+
+            using (GhostscriptRasterizer _rasterizer = new GhostscriptRasterizer())
+            {
+                _rasterizer.Open(options.Input, options.Gvi, true);
+
+                for (int pageNr = 1; pageNr <= _rasterizer.PageCount; pageNr++)
+                {
+                    string dest = Path.Combine(options.OutputPath, $"{options.DestFileNameWithoutExtension}-{pageNr}.png");
+                    Image img = _rasterizer.GetPage(options.Desired_X_dpi, options.Desired_Y_dpi, pageNr);
+                    img.Save(dest, ImageFormat.Png);
+
+                    //upload to azure
+                    //http://lightswitchhelpwebsite.com/Blog/tabid/61/EntryId/3303/Convert-PDF-files-to-PNG-Images-using-Azure-Functions.aspx
+                    //file.delete
+                }
             }
 
-            return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+            File.Delete(options.Input);
+
+            return req.CreateResponse(HttpStatusCode.OK, "Hello");
+        }
+
+        private static string GetInputFileFromUrl(HttpRequestMessage req)
+        {
+            //TODO: invalid/missing input
+            return req.GetQueryNameValuePairs()
+                .FirstOrDefault(q => string.Compare(q.Key, "input", true) == 0)
+                .Value;
+        }
+
+        private static string DownloadFile(string input)
+        {
+            string res = $"c:\\temp\\{Guid.NewGuid()}.pdf"; //TODO temp
+
+            using (var wc = new WebClient())
+            {                
+                wc.DownloadFile(input, res);
+            }
+
+            return res;
         }
     }
 }
---
+++ pdf2png/Options.cs
@@ -0,0 +1,25 @@
+﻿using Ghostscript.NET;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pdf2png
+{
+    public class Options
+    {
+        public string Input { get; set; }
+
+        public string OutputPath => Path.GetDirectoryName(Input); //TODO
+
+        public string DestFileNameWithoutExtension => Path.GetFileNameWithoutExtension(Input); //TODO
+
+        public int Desired_Y_dpi { get; set; } = 96;
+
+        public int Desired_X_dpi { get; set; } = 96;
+
+        public GhostscriptVersionInfo Gvi { get; } = new GhostscriptVersionInfo(Path.GetFullPath(@"..\..\..\..\3rdParty\gsdll32.dll"));
+    }
+}
